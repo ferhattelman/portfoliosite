31b5c8e2292f7da316b6dca06eaca22d457fd5da
ferhattelman <111305718+ferhattelman@users.noreply.github.com>
2023-11-08 19:24:09 +0300

    update

--- js/script.js
+++ js/script.js
@@ -24,6 +24,11 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
         }
     });
 
@@ -36,5 +41,7 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 
     // animation footer on scroll
-    
+    let footer = document.querySelector('footer')
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
